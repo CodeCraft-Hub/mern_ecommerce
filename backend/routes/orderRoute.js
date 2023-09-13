a9cfa3a5218f0087217c29469ff54deaeae2d321
Code Craft Hub <mycodecrafthub.com>
2023-09-12 21:17:11 -0700

    changes

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -4,9 +4,7 @@ const {addOrderItem} = require('../controllers/orderController')
 const {protect} = require("../middlewares/authMiddleware")
 
 // create new route
-// router.route("/").post(protect, addOrderItem);
-
-
+router.post("/",(req,res)=> addOrderItem)
 
 
 module.exports = router;
